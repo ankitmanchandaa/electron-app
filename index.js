957d185c1447244c81a2715a13b3b20ecdf8f0e8
Ankit Manchanda <ankitmanchandaa@gmail.com>
2020-08-18 13:15:11 +0530

    vunerablity fixes

--- index.js
+++ index.js
@@ -1,73 +1,62 @@
-const {app, BrowserWindow, Menu} = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 
-let win;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
-// const menuTemplate = [
-//     {
-//         label: 'Electron',
-//         submenu: [
-//             {
-//                 label: 'About ...',
-//                 click: () => {
-//                     console.log('About Clicked');
-//                 }
-//             }, {
-//                 type: 'separator'
-//             }, {
-//                 label: 'Quit',
-//                 click: () => {
-//                     app.quit();
-//                 }
-//             }
-//         ]
-//     }
-// ];
+function createWindow() {
+  // Create the browser window.
+  win = new BrowserWindow({
+      width: 1000,
+      height: 800,
+      webPreferences: {
+          nodeIntegration: true
+      },
+      icon: './assets/ankit.png'
+  })
 
-const createWindow = () => {
-    // Create the browser window.
-    win = new BrowserWindow({
-        width: 350,
-        height: 270
-    })
+  // and load the index.html of the app.
+  // win.loadFile('index.html')
+  win.loadURL('https://ankitmanchandaa.github.io/portfolio')
 
-    // and load the index.html of the app.
-    // win.loadFile(`file://${__dirname}/public/dist/index.html`)
-    // Open the DevTools.
-    // win.webContents.openDevTools()
-    win.loadURL(`file://${__dirname}/public/dist/index.html`)
-    // const menu = Menu.buildFromTemplate(menuTemplate);
-    // Menu.setApplicationMenu(menu);
-    // win.setMenu(menu)
-
-
-    
-
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null
-    })
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+      // Dereference the window object, usually you would store windows
+      // in an array if your app supports multi windows, this is the time
+      // when you should delete the corresponding element.
+      win = null
+  })
 }
 
-// const menu = Menu.buildFromTemplate(menuTemplate)
-// Menu.setApplicationMenu(menu)
-app.on('ready', createWindow)
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  require('./settings/menu').configure()
+  createWindow()
+})
 
-
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
       app.quit()
-    }
-  })
-  
-  app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
       createWindow()
-    }
-  })
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
